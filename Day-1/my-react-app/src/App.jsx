c823b31910ff726035d17653c13c80d7760887a0
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-05-04 09:33:10 +0530

    Initialize my-react-app with Vite, React, and Tailwind CSS
    - Created package.json with dependencies and scripts for development, build, linting, and preview.
    - Added App.css for styling with responsive design and animations.
    - Implemented App.jsx as the main component, integrating Fixed and Move components.
    - Created index.css to include Tailwind CSS and custom font styling.
    - Set up main.jsx to render the App component within StrictMode.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ Day-1/my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Fixed from './components/Fixed'
+import Move from './components/Move'
+const App = () => {
+  return (
+    <div >
+      <Fixed />
+      <Move/>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
