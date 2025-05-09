705987a3615343619fa1c6af1bb4cb6e69af2d53
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-05-09 10:54:49 +0530

    feat: add Footer component; implement footer structure with About, Links, and Privacy sections

---
+++ Day-2/my-react-app/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="">
+       <footer class="footer">
+    <div class="footer-column">
+      <h3>About</h3>
+      <p>Studio—VW®</p>
+      <p>Vedant Wedekar</p>
+    </div>
+    <div class="footer-column">
+      <h3>Links</h3>
+      <p>Home</p>
+      <p>Projects</p>
+      <p>Contact</p>
+    </div>
+    <div class="footer-column">
+      <h3 class="hover-trigger">Privacy</h3>
+      <div class="privacy-content">
+        <p>This website respects your privacy. No cookies are used. Your data stays yours.</p>
+      </div>
+      <h3 class="hover-trigger">Privacy</h3>
+      <div class="privacy-contentt">
+        <p>This website respects your privacy. No cookies are used. Your data stays yours.</p>
+      </div> <h3 class="hover-trigger">Privacy</h3>
+      <div class="privacy-content">
+        <p>This website respects your privacy. No cookies are used. Your data stays yours.</p>
+      </div> <h3 class="hover-trigger">Privacy</h3>
+      <div class="privacy-content">
+        <p>This website respects your privacy. No cookies are used. Your data stays yours.</p>
+      </div>
+    </div>
+  </footer>
+    </div>
+  )
+}
+
+export default Footer
+
+
+
+
+
+
+
