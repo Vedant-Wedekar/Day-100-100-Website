be5de0215f0faea354f4fc97400028a5f4caa404
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-05-13 13:27:03 +0530

    feat: add Cyl component with textured cylinder geometry and rotation animation

---
+++ Day-7/my-react-app/src/Cyl.jsx
@@ -0,0 +1,21 @@
+import React , { useRef } from 'react'
+import {  useTexture } from '@react-three/drei'
+import * as THREE from 'three'
+import { useFrame } from '@react-three/fiber'
+const Cyl = () => {
+      let tex = useTexture('./src/assets/image.png')
+      let Cyl = useRef(null);
+      useFrame((state,delta) => {
+    Cyl.current.rotation.y += delta     
+      });
+  return (
+         <mesh ref={Cyl} rotation={[0.1,1,0.2]}   >
+            <cylinderGeometry  args={[1,1,1,30,30,true]} />
+            <meshStandardMaterial map={tex}  side={ THREE.DoubleSide } />
+            </mesh>
+  )
+}
+export default Cyl
+
+
+
