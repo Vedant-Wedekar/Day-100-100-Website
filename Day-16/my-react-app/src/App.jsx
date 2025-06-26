2d4443ebf8f12afa38a7600c1c2962881d935fbc
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-06-26 23:23:13 +0530

    feat: add Login component for user authentication

--- Day-16/my-react-app/src/App.jsx
+++ Day-16/my-react-app/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Hero from './Components/Hero'
+import Login from './Components/Login'
 const App = () => {
   return (
     <div>
+      <Login />
       <Hero />
     </div>
   )
