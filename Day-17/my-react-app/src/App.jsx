c1086ab0e037a50eff90a0ecc2640ee6f009ae9e
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-06-27 20:59:54 +0530

    refactor: update App component structure to include Header, Footer, Main, Cookies, Main2, and Contact components

--- Day-17/my-react-app/src/App.jsx
+++ Day-17/my-react-app/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/Main'
+import Cookies from './components/Cookies'
+import Main2 from './components/Main2'
+import Contact from './components/Contact'
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Main />
+      <Main2 />
+      <Footer />
+      <Cookies />
+      <Contact />
+    </>
   )
 }
 
 export default App
+
+
+
+
