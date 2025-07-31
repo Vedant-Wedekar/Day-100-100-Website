9ef6fd0cf4359805eeb95d3fd0a991eb86304efc
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-07-31 10:23:32 +0530

    update 1 file and create 9 files

--- Day-14/my-react-app/src/App.jsx
+++ Day-14/my-react-app/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const App = () => {
+
+  function handleclick()
+  {
+    
+    alert("Hey Anushka");
+
+
+  }
+  function handhower()
+  {
+    alert("Hey user");
+  }
   return (
     <div>
       
+      <button onClick={handleclick}>Click Me!</button>
+      <button onMouseOver={handhower}>Click</button>
     </div>
   )
 }
