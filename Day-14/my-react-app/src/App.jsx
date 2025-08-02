062887dc12da65c1966d1039a5b836a10e7448aa
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-08-02 13:29:15 +0530

    update 1 file and create 5 files

--- Day-14/my-react-app/src/App.jsx
+++ Day-14/my-react-app/src/App.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const App = () => {
+  const colors = ["#ff0000", "#00ff00", "#0000ff"];
+  const [color, setColor] = useState(colors[0]); // fix 1
 
-  function handleclick()
-  {
-    
-    alert("Hey Anushka");
-
-
+  function chngmode() {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)]; // fix 2
+    setColor(randomColor); // fix 3
   }
-  function handhower()
-  {
-    alert("Hey user");
-  }
+
   return (
-    <div>
-      
-      <button onClick={handleclick}>Click Me!</button>
-      <button onMouseOver={handhower}>Click</button>
+    <div style={{ backgroundColor: color, height: "1000px", width: "1000px" }}>
+      <button
+        style={{ backgroundColor: "white", height: "100px", width: "100px" }}
+        onClick={chngmode}
+      >
+        Click ME
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+
+
+
+  //  const randomColor = colors[Math.floor(Math.random() * colors.length)];
