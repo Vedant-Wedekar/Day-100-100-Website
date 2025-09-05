097a82880800f0e69a8b20811a4492b73cc9afb5
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-09-05 11:17:47 +0530

    create 18 files

---
+++ Day-23/my-react-app/src/components/Main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import BG from '../assets/BG.jpg'
+const Main = () => {
+  return (
+    <div       style={{
+        backgroundImage: `url(${BG})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        width: '100%'
+      }} >
+
+
+
+
+
+
+
+
+
+
+
+
+        
+      </div>
+  )
+}
+
+export default Main
