f87dfb856d369a5893a11a129b021a8844a5e250
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-09-05 18:24:38 +0530

    update Day-23/my-react-app/index.html, Main.jsx, Day-23/my-react-app/src/index.css and vite.config.js

--- Day-23/my-react-app/vite.config.js
+++ Day-23/my-react-app/vite.config.js
@@ -5,5 +5,20 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
         tailwindcss(),
+         function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-shadow': {
+          textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+        },
+        '.text-shadow-md': {
+          textShadow: '4px 4px 6px rgba(0, 0, 0, 0.5)',
+        },
+        '.text-shadow-lg': {
+         textShadow: '100px 180px 240px rgba(0, 0, 0, 50%)',
+
+        },
+      }
+      addUtilities(newUtilities, ['responsive', 'hover'])
+    },
   ],
 })
