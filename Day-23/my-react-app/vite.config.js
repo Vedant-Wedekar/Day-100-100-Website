a56fe14cedf91301a30702d7d0553a08b1b499e6
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-09-07 12:42:55 +0530

    create 3 files and update 2 files

--- Day-23/my-react-app/vite.config.js
+++ Day-23/my-react-app/vite.config.js
@@ -1,24 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-        tailwindcss(),
-         function ({ addUtilities }) {
-      const newUtilities = {
-        '.text-shadow': {
-          textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
-        },
-        '.text-shadow-md': {
-          textShadow: '4px 4px 6px rgba(0, 0, 0, 0.5)',
-        },
-        '.text-shadow-lg': {
-         textShadow: '100px 180px 240px rgba(0, 0, 0, 50%)',
-
-        },
-      }
-      addUtilities(newUtilities, ['responsive', 'hover'])
-    },
+ 
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
 })
+
+
+
+
+//  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         myfont: ['Dog', 'sans-serif'],
+//       },
+//     },
+//   },
