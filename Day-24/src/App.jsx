ff1e9a0fa2a28eb464f589a68cbc2ae652d7fbd9
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-09-16 05:52:26 +0530

    Refactor code structure for improved readability and maintainability

--- Day-24/src/App.jsx
+++ Day-24/src/App.jsx
@@ -9,6 +9,15 @@ import FullScreenNav from './components/Navigation/FullScreenNav'
 const App = () => {
   return (
     <div className='overflow-x-hidden'>
+      
+     <div
+        className="gradient-blob w-[700px] h-[600px] right-[-10rem] top-80"
+        style={{
+          background: "radial-gradient(circle, #afc5f7, transparent 70%)",
+        }}
+        aria-hidden="true"
+      ></div>
+
       <Navbar />
       <FullScreenNav />
       <Routes>
@@ -16,6 +25,13 @@ const App = () => {
         <Route path='/agence' element={<Agence />} />
         <Route path='/projects' element={<Projects />} />
       </Routes>
+       <div
+        className="gradient-blob w-[600px] h-[700px] -left-40 -top-32"
+        style={{
+          background: "radial-gradient(circle, #d5b1f4, transparent 70%)",
+        }}
+        aria-hidden="true"
+      ></div>
     </div>
   )
 }
