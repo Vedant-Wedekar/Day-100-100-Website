42c9816f50d676d72653e5293ffbf2d779dee0ea
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-12-01 17:42:39 +0530

    feat: Refactor App component and add Clock, Dots, ActionBtn, and Mode components

---
+++ Day-27/Ved-app/src/components/Clock.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Clock = () => {
+  return (
+    <div>
+
+
+        <div className="bg-white h-100 w-100 rounded-full  flex  justify-center items-center  ">
+            <div className="bg-[#81a2eb] h-94 w-94 rounded-full flex justify-center items-center ">     </div>
+        </div>
+
+        
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Clock
---
+++ Day-27/Ved-app/src/components/Dots.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dots = () => {
+  return (
+    <div>
+
+<div className="h-1 w-1 bg-black"></div>
+    </div>
+  )
+}
+
+export default Dots
