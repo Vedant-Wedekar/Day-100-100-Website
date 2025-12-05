00bf263eacd482de51fd1c57ff2f92ca536bd182
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-12-05 13:44:48 +0530

    feat: Update Dots component layout with flexbox for improved spacing

--- Day-27/Ved-app/src/components/Dots.jsx
+++ Day-27/Ved-app/src/components/Dots.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const Dots = () => {
   return (
-    <div>
+    <div className='flex  gap-2'>
+<div className="flex">
+<div className="h-3 w-3 bg-black rounded-2xl"></div>
+<div className="h-3 w-3 bg-black rounded-2xl"></div>
+<div className="h-3 w-3 bg-black rounded-2xl"></div>
+<div className="h-3 w-3 bg-black rounded-2xl"></div>
+</div>
+<div className="flex">
+<div className="h-3 w-3 bg-black rounded-2xl"></div>
+<div className="h-3 w-3 bg-black rounded-2xl"></div>
+<div className="h-3 w-3 bg-black rounded-2xl"></div>
+<div className="h-3 w-3 bg-black rounded-2xl"></div>
+</div>
 
-<div className="h-1 w-1 bg-black"></div>
     </div>
   )
 }
